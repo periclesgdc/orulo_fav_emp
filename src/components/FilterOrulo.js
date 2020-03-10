fa58046538610ba4cf352a4ea80fe8f3a307cd27
Pericles <pericles.costa@tecnotech.org>
2020-03-10 15:02:04 -0300

    #Novas alteraçoes no layout

--- src/components/FilterOrulo.js
+++ src/components/FilterOrulo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Form, Button, Row, Col } from 'react-bootstrap';
+import { Card, Form, InputGroup, Button, Row, Col } from 'react-bootstrap';
 
 export default class FilterOrulo extends Component {
   constructor(props) {
@@ -21,23 +21,30 @@ export default class FilterOrulo extends Component {
   render() {
     return(
       <Card className="p-3">
-        <Form style={}>
-          <Form.Group controlId="formBasicCity">
-            <Form.Label>Cidade</Form.Label>
-            <Form.Control type="text" placeholder="Ex.: Belo Horizonte, MG" onKeyUp={this.loadCities.bind(this)} />
-          </Form.Group>
+        <Form>
+          <InputGroup className="sm-3">
+            <InputGroup.Prepend size="sm">
+              <InputGroup.Text id="city">Cidade</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              type="text"
+              placeholder="Ex.: Belo Horizonte, MG"
+              onKeyUp={this.loadCities.bind(this)}
+            />
+          </InputGroup>
 
           <Form.Group controlId="formBasicArea">
-            <Form.Label>Bairro</Form.Label>
-            <Form.Control type="text" placeholder="Informe o bairo" disabled={this.state.disableArea} />
+            <Form.Label size="sm">Bairro</Form.Label>
+            <Form.Control size="sm" type="text" placeholder="Informe o bairo" disabled={this.state.disableArea} />
           </Form.Group>
 
           <Form.Group as={Row} controlId="formBasicOpportunity">
             <Col sm="5">
-              <Form.Label>Oportunidade para</Form.Label>
+              <Form.Label size="sm">Oportunidade para</Form.Label>
               <div key="default-checkbox" className="mb-3">
                 {this.state.opportunities.map(obj => (
                   <Form.Check
+                    size="sm"
                     type={"checkbox"}
                     name={Object.keys(obj)[0]}
                     id={Object.keys(obj)[0]}
@@ -49,8 +56,8 @@ export default class FilterOrulo extends Component {
           </Form.Group>
 
           <Form.Group controlId="formBasicOpportunity">
-            <Form.Label>Status do empreendimento</Form.Label>
-            <Form.Control as="select">
+            <Form.Label size="sm">Status do empreendimento</Form.Label>
+            <Form.Control size="sm" as="select">
               <option value="under_construction">Em construção</option>
               <option value="ready">Construído</option>
               <option value="used">Usado</option>
@@ -58,28 +65,28 @@ export default class FilterOrulo extends Component {
           </Form.Group>
 
           <Form.Group controlId="formBasicFinality">
-            <Form.Label>Finalidade do empreendimento</Form.Label>
-            <Form.Control as="select">
+            <Form.Label size="sm">Finalidade do empreendimento</Form.Label>
+            <Form.Control size="sm" as="select">
               <option value="comercial">Comercial</option>
               <option value="residencial">Residencial</option>
             </Form.Control>
           </Form.Group>
 
           <Form.Group controlId="formBasicType">
-            <Form.Label>Tipo do imóvel</Form.Label>
-            <Form.Control as="select">
+            <Form.Label size="sm">Tipo do imóvel</Form.Label>
+            <Form.Control size="sm" as="select">
               <option value="tipo">tipo</option>
             </Form.Control>
           </Form.Group>
 
           <Form.Group controlId="formBasicBedroom">
-            <Form.Label>Quantidade de quartos</Form.Label>
-            <Form.Control as="select">
+            <Form.Label size="sm">Quantidade de quartos</Form.Label>
+            <Form.Control size="sm" as="select">
               <option value="quantidade">quantidade</option>
             </Form.Control>
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" size="sm">
             Submit
           </Button>
         </Form>
